c4bba68f02edd91c1c7cdce9f1915ed610519d80
pmendy-teroola <teroolaphoto@gmail.com>
2026-05-20 01:57:00 +0000

    Add renderPOS function to display development message

--- modules/pos.js
+++ modules/pos.js
@@ -1,1 +1,3 @@
-
+function renderPOS(main) {
+  main.innerHTML = '<div class="card"><div style="color:var(--text-secondary);text-align:center;padding:40px">Module Caisse - en cours de developpement</div></div>';
+}
