f63932c5ff2e6d398e6ac2096aa65aafb5dc0ec0
pmendy-teroola <teroolaphoto@gmail.com>
2026-05-20 02:01:30 +0000

    Add renderUsers function to display user module

---
+++ modules/users.js
@@ -0,0 +1,3 @@
+function renderUsers(main) {
+  main.innerHTML = '<div class="card"><div style="color:var(--text-secondary);text-align:center;padding:40px">Module Utilisateurs - en cours de developpement</div></div>';
+}
