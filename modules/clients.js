fabd5b093ed23c245f3fa08f9d531c458b912c62
pmendy-teroola <teroolaphoto@gmail.com>
2026-05-20 01:59:47 +0000

    Add renderClients function to display client module

---
+++ modules/clients.js
@@ -0,0 +1,3 @@
+function renderClients(main) {
+  main.innerHTML = '<div class="card"><div style="color:var(--text-secondary);text-align:center;padding:40px">Module Clients - en cours de developpement</div></div>';
+}
