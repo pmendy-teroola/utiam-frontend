b1320abf0be11f5a95230669669204f6cf3a870e
pmendy-teroola <teroolaphoto@gmail.com>
2026-05-20 02:00:40 +0000

    Add renderReports function to display development message

---
+++ modules/reports.js
@@ -0,0 +1,3 @@
+function renderReports(main) {
+  main.innerHTML = '<div class="card"><div style="color:var(--text-secondary);text-align:center;padding:40px">Module Rapports - en cours de developpement</div></div>';
+}
